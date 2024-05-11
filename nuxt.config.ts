252720a493f8aaa2bc24301e2f7d3e0a894c99ba
EDSPower <766782971@qq.com>
2024-05-11 13:51:45 +0800

    fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,28 @@
 /*
  * @Author: EDSPower
  * @Date: 2024-05-07 20:13:31
- * @LastEditTime: 2024-05-11 13:37:17
+ * @LastEditTime: 2024-05-11 13:48:39
  * @LastEditors: EDSPower
  * @FilePath: \vue-nuxt\nuxt.config.ts
  * 766782971@qq.com
  */
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'path'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'docs')
+    }
+  },
   modules: ["@unocss/nuxt", "@pinia/nuxt", "nuxt-icon"],
   css: ["~/assets/scss/global.scss"],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '$fontColor1: #111827; $fontColor2: #6b7280;',
+          additionalData: '@import "@/assets/scss/_var.scss";',
         },
       },
     },
